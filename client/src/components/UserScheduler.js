afbe667d1f5d6307e0a9a8ffc769cb50f02463f3
randze <65006180+randze@users.noreply.github.com>
2020-07-30 13:55:40 -0400

    Updated folder structure

--- client/src/components/UserScheduler.js
+++ client/src/components/UserScheduler.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function UserScheduler(){
-    return(<>
-    User Scheduler
-    </>)
+    return(
+    <>
+        User Scheduler
+    </>
+    )
 }
 
 export default UserScheduler
