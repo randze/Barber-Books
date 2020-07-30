86616f41eb6f8a15ae82fafc3a5a966908b4b3d4
randze <65006180+randze@users.noreply.github.com>
2020-07-30 14:02:17 -0400

    Added server.js and run dev commands

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require('mongoose')
+const path = require("path");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+// express
+const PORT = process.env.PORT || 8080
+app.use(express.urlencoded({ extended: true }))
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/scheduler',
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+
+// app listener
+app.listen(PORT, function () {
+    console.log(`Serving app on: http://localhost:${PORT}`)
+})
+  
