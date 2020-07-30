af522f66fdb16b5430199ebc9b64ffd7c17c2ff5
ense <eddi.n.esteban@gmail.com>
2020-07-30 13:51:00 -0400

    test calendar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {Switch, Route, Link} from 'react-router-dom'
 import UserScheduler from './components/UserScheduler'
 import VendorScheduler from './components/VendorScheduler'
 import Login from './components/Login'
+import Calendar from 'rc-calendar'
+import 'rc-calendar/assets/index.css';
 
 function App() {
   return (
@@ -11,10 +13,11 @@ function App() {
       <Link to='/'>Home</Link>
       <Link to='/about'>About</Link>
       <div>
-      <Switch>
-        <Route path='/'><Login/></Route>
+      <Switch> {/*more specific paths first*/}
         <Route path='/about'><VendorScheduler/></Route>
+        <Route path='/'><Login/></Route>
       </Switch>
+      <Calendar/>
       </div>
     </div>
   );
