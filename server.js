893e4c1c79b5a9c313062984e999cda27b21df6a
randze <65006180+randze@users.noreply.github.com>
2020-07-30 14:25:57 -0400

    Updated package.json

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require("express");
-const mongoose = require('mongoose')
 const path = require("path");
+const PORT = process.env.PORT || 3001;
 const app = express();
-
-// express
-const PORT = process.env.PORT || 8080
-app.use(express.urlencoded({ extended: true }))
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// mongoose
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/scheduler',
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-// app listener
-app.listen(PORT, function () {
-    console.log(`Serving app on: http://localhost:${PORT}`)
-})
-  
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
