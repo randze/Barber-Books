7a4d673a5904a2eb58d9d7d67e5a610951b62a7a
randze <65006180+randze@users.noreply.github.com>
2020-08-06 14:28:51 -0400

    Added footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Footer() {
 	return (
-		<div>
-			<p>by Team EPA</p>
-		</div>
+		<footer className="footer text-center align-middle">
+			<span>by Team EPA &trade;</span>
+		</footer>
 	)
 }
 
